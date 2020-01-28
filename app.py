a2ad51a2fc81d07052f711ebcb8847300c672fc1
Ovi <Chrono.hi@gmail.com>
2020-01-28 13:30:03 -0500

    fix: Erase unnecessary code after previous commit

--- app.py
+++ app.py
@@ -10,59 +10,21 @@ from gevent.pywsgi import WSGIServer
 import tensorflow as tf
 from tensorflow import keras
 
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-
 # Some utilites
 import numpy as np
-#from util import base64_to_pil
 
-#from model.bottleneck.extract_bottleneck_features import *
-#from model.detectors.dog_detector import *
-#from model.detectors.face_detector import *
-from NeuralNetwork import *
+from neural_network import *
 
 # Declare a flask app
 app = Flask(__name__)
 
 
-# You can use pretrained model from Keras
-# Check https://keras.io/applications/
-from keras.applications.mobilenet_v2 import MobileNetV2
-model = MobileNetV2(weights='imagenet')
-
 ###
-weight_path = 'model/saved_model/weights.best.from_VGG19.hdf5'
-neural = NeuralNetwork(weight_path)
+neural = NeuralNetwork()
 ###
 
 print('Model loaded. Check http://127.0.0.1:5000/')
 
-
-# Model saved with Keras model.save()
-MODEL_PATH = 'models/your_model.h5'
-
-# Load your own trained model
-# model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-
-def model_predict(img, model):
-    img = img.resize((224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='tf')
-
-    preds = model.predict(x)
-    return preds
 
 
 @app.route('/', methods=['GET'])
@@ -74,21 +36,6 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        # Get the image from post request
-        #img = base64_to_pil(request.json)
-
-        # Save the image to ./uploads
-        # img.save("./uploads/image.png")
-
-        # Make prediction
-        #preds = model_predict(img, model)
-
-        # Process your result for human
-        #pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-        #pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-
-        #result = str(pred_class[0][0][1])               # Convert to string
-        #result = result.replace('_', ' ').capitalize()
 
         pred_proba = 1
         result2 = neural.predict_breed('model/uploaded_images/test.jpg')
